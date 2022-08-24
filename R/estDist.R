3190b528528b1a40db37533450162eb83240ed56
Biuw <a5406@hi.no>
2022-08-24 17:45:28 +0200

    Initial commit

--- R/estDist.R
+++ R/estDist.R
@@ -1,3 +1,19 @@
+#' \code{estDistance} Estimate distance to sighting
+#'
+#' @param brH Height of observation platform (in meters)
+#' @param obsH Height of observer (in meters)
+#' @param angle Angle of depression (pitch) recorded by e.g. SensorLogger
+#' @return Estimated distance to sighting (in meters)
+#' @details To be added
+#' @family Sightings survey data processing
+#' @seealso \code{\link{readSL}} to read single observation data in a subdirectory,
+#'   \code{\link{getSightings}} to read all observations in a main directory,
+#'   \code{\link{annotateSightings}} to manually extract information from voice recordings,
+#' @author Martin Biuw
+#' @example
+#' To be added
+#' @export
+#'
 estDistance <- function(brH=19.2, obsH=1.73, angle=-0.4055861) {
   daHoriz <- function(br=brH, h=obsH, r=6378137) {
     ro <- r+br+h
@@ -5,9 +21,9 @@ estDistance <- function(brH=19.2, obsH=1.73, angle=-0.4055861) {
     a <- circular::deg(atan2(d, r))
     c(dh=d, ah=a, arev=90-a)
   }
-  
+
   dH <- daHoriz()
-  
+
   findDist <- function(br=brH, h=obsH, r=6378137, Arev=90-angle, aHrev=dH[3], aD=dH[1]) {
     totH <- br+h+r
     stepSize <- round(diff(seq(1, totH, length=1000))[1])
@@ -15,18 +31,18 @@ estDistance <- function(brH=19.2, obsH=1.73, angle=-0.4055861) {
     oppH <- aD*sin(circular::rad(aHrev))
     maxAdj <- sqrt(aD^2-oppH^2)
     minAdj <- br+h
-    
-    
+
+
     findHyp <- function(x=seq(minAdj, maxAdj, by=0.01)) {
       opp <- x*tan(circular::rad(Arev))
       xx <- totH-x
       sqrt(opp^2+xx^2)
     }
-    
+
     optAdj <- which.min(abs(r-findHyp()))
     optAdj <- seq(minAdj, maxAdj, by=0.01)[optAdj]
     optOpp <- optAdj*tan(circular::rad(Arev))
-    optD <- sqrt(optAdj^2+optOpp^2)
+    sqrt(optAdj^2+optOpp^2)
   }
-  findDist()  
+  findDist()
 }
--- R/readSL.R
+++ R/readSL.R
@@ -1,53 +1,93 @@
+#' \code{readSL} Read data from single SensorLogger observation directory
+#'
+#' @param subDir name of the subdirectory containing data from a single observation instance
+#' @return Returns a list with all sensor data, except for the voice recording
+#' Also includes an added data frame with all relevant extracted sightings sensor information
+#' @details To be added
+#' @family Sightings survey data processing
+#' @seealso \code{\link{getSightings}} to read all observations in a main directory,
+#'   \code{\link{annotateSightings}} to manually extract information from voice recordings,
+#'   \code{\link{estDist}} to estimate distance to sightings,
+#' @author Martin Biuw
+#' @example
+#' sl <- readLSL()
+#' @export
+
+
 readSL <- function(subDir='2021-12-14_12-20-38') {
   options(warn=-1)
-  unzip(paste('./SensorLogger/', subDir, '.zip', sep=''), exdir=paste('./SensorLogger/', subDir, sep=''))
-  file.list <- dir(paste('./SensorLogger/', subDir, sep=''))
+  unzip(paste(subDir, '.zip', sep=''), exdir=subDir)
+  file.list <- dir(subDir)
   csv.list <- file.list[grep('csv', file.list)]
-  
+
   sensor.list <- list()
-  
+
   for(i in 1:length(csv.list)) {
-    csv.file <- paste('./SensorLogger/', subDir, '/', csv.list[i], sep='')
-    sensor.list[[i]] <- read.csv(csv.file)   
+    csv.file <- paste(subDir, '/', csv.list[i], sep='')
+    sensor.list[[i]] <- try(read.csv(csv.file), silent=T)
   }
   names(sensor.list) <- gsub('.csv', '', csv.list)
   options(warn=0)
   sensor.list <- sensor.list[c(match('Metadata', names(sensor.list)),
-                               setdiff(c(1:length(sensor.list)), 
+                               setdiff(c(1:length(sensor.list)),
                                        match('Metadata', names(sensor.list))))]
-  
-  
+
+
   t.digits <- max(nchar(round(1000/as.numeric(unlist(strsplit(sensor.list$Metadata$sampleRateMs, '|', fixed=T))))))
-  options(digits.secs=t.digits)    
+  options(digits.secs=t.digits)
   for(i in 2:length(sensor.list)) {
-    sensor.list[[i]]$time <- as.POSIXct(sensor.list[[i]]$time/1000000000, 
-                                        origin='1970-01-01 00:00:00', 
-                                        tz='UTC')
+    if(class(sensor.list[[i]])!='try-error') {
+      sensor.list[[i]]$time <- as.POSIXct(sensor.list[[i]]$time/1000000000,
+                                          origin='1970-01-01 00:00:00',
+                                          tz='UTC')
+    }
   }
-  
-  sensor.list$Magnetometer$bearing <- -atan2(sensor.list$Magnetometer$x, sensor.list$Magnetometer$y)*(180/pi)
-  sensor.list$Magnetometer$bearing[which(sensor.list$Magnetometer$bearing<0)] <- 
-    sensor.list$Magnetometer$bearing[which(sensor.list$Magnetometer$bearing<0)] + 360
-  
-  angle <- sensor.list$Magnetometer$bearing-mean(sensor.list$Location$bearing[-1])
-  angle[which(angle>180)] <- angle[which(angle>180)]-360
-  pitch <- sensor.list$Orientation$pitch
-  sensor.list$sighting <- list(angle=data.frame(time=sensor.list$Magnetometer$time,
-                                                angle=angle), 
-                               pitch=data.frame(time=sensor.list$Orientation$time,
-                                                pitch=pitch))
-  unlink(paste('./SensorLogger/', subDir, sep=''), recursive = TRUE)
+
+  if(class(sensor.list$Magnetometer)!='try-error') {
+    sensor.list$Magnetometer$bearing <- -atan2(sensor.list$Magnetometer$x, sensor.list$Magnetometer$y)*(180/pi)
+    sensor.list$Magnetometer$bearing[which(sensor.list$Magnetometer$bearing<0)] <- sensor.list$Magnetometer$bearing[which(sensor.list$Magnetometer$bearing<0)] + 360
+  }
+
+  if(class(sensor.list$Magnetometer)!='try-error' & class(sensor.list$Location)!='try-error') {
+    angle <- sensor.list$Magnetometer$bearing-mean(sensor.list$Location$bearing[-1])
+    angle[which(angle>180)] <- angle[which(angle>180)]-360
+    angle <- data.frame(time=sensor.list$Magnetometer$time,
+                        angle=angle)
+  } else {
+    angle <- NA
+  }
+
+  if(class(sensor.list$Orientation)!='try-error') {
+    pitch <- data.frame(time=sensor.list$Orientation$time, pitch=sensor.list$Orientation$pitch)
+  }
+
+  sensor.list$sighting <- list(angle=angle,
+                               pitch=pitch)
+  unlink(subDir, recursive = TRUE)
   sensor.list
 }
 
 
+#' \code{getSightings} Read all observations in a main directory
+#'
+#' @param mainDir name of the  main directory containing all observation data from a cruise
+#' @return Returns a list with the sightings component from all observations
+#' @details To be added
+#' @family Sightings survey data processing
+#' @seealso \code{\link{readSL}} to read single observation data in a subdirectory,
+#'   \code{\link{annotateSightings}} to manually extract information from voice recordings,
+#'   \code{\link{estDist}} to estimate distance to sightings,
+#' @author Martin Biuw
+#' @example
+#' To be added
+#' @export
 
 getSightings <- function(mainDir='C:/Users/a5406/Documents/OneOcean/SensorLogger') {
-  require(progress)  
+  require(progress)
   oDir <- getwd()
   setwd(mainDir)
   subDirs <- dir()
-  
+
   sightings <- list()
   pb <- progress_bar$new(total=length(subDirs))
   for(i in 1:length(subDirs)) {
@@ -59,25 +99,39 @@ getSightings <- function(mainDir='C:/Users/a5406/Documents/OneOcean/SensorLogger
       sightings[[i]] <- sl$sighting
     } else {
       sightings[[i]] <- sl
-    }  
+    }
   }
   names(sightings) <- gsub('.zip', '', subDirs)
   sightings
 }
 
+#' \code{annotateSightings} Extract information from voice recording
+#'
+#' @param mainDir name of the  main directory containing all observation data from a cruise
+#' @return To be added
+#' @details To be added
+#' @family Sightings survey data processing
+#' @seealso \code{\link{readSL}} to read single observation data in a subdirectory,
+#'   \code{\link{getSightings}} to read all observations in a main directory,
+#'   \code{\link{estDist}} to estimate distance to sightings,
+#' @author Martin Biuw
+#' @example
+#' To be added
+#' @export
+#'
 annotateSightings <- function(mainDir='C:/Users/a5406/Documents/OneOcean/SensorLogger') {
   require(av)
   oDir <- getwd()
   setwd(mainDir)
   subDirs <- dir()
-  
+
   for(i in 1:length(subDirs)) {
     exDir <- gsub('.zip', '', subDirs[i])
     unzip(subDirs[i], exdir=exDir)
     av_spectrogram_video(audio=paste('./', exDir, '/Microphone.m4a', sep=''),
                         output=paste('./', exDir, '/Recording.mp4', sep=''))
     utils::browseURL(paste('./', exDir, '/Microphone.m4a', sep=''))
-    
+
     setwd(paste('./', exDir, sep=''))
     dir.create('./www')
     file.copy('Microphone.m4a', './www/Microphone.m4a')
